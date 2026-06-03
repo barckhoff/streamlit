694b74f7b8cecc3263f87146c38e4815e9585ebc
barckhoff <barckhoff@users.noreply.github.com>
2026-06-03 16:33:29 -0400

    Update finanalysis.py

--- finanalysis.py
+++ finanalysis.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import pandas as pd
 import streamlit as st
-import datetime
+import datetime as dt
 
 def fetch_data(tickers, start_date, end_date):
     data_frames = []
@@ -16,7 +16,7 @@ def fetch_data(tickers, start_date, end_date):
 st.title("Financial Analysis Web App")
 
 # User inputs
-today = datetime.today().strftime('%Y-%m-%d')
+today = dt.today().strftime('%Y-%m-%d')
 tickers = st.text_input("Enter stock tickers (comma separated)", "AAPL, MSFT, GOOG").split(",")
 tickers = [ticker.strip() for ticker in tickers]
 start_date = st.date_input("Start Date", pd.to_datetime("2026-01-01"))
