93da13a41ca5932cb2229e365142d675e424bd2f
barckhoff <barckhoff@users.noreply.github.com>
2026-06-03 14:42:42 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -15,20 +15,19 @@ for message in st.session_state.messages:
 with st.container():
     # React to user input
     if prompt := st.chat_input("What is up?"):
-        with st.container():
-            # Display user message in chat message container
-            st.chat_message("user").markdown(prompt)
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
         
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
         
-            response = f"Echo: {prompt}"
-            # Display assistant response in chat message container
+        response = f"Echo: {prompt}"
+        # Display assistant response in chat message container
             
-            with st.chat_message("assistant"):
-                st.markdown(response)
+        with st.chat_message("assistant"):
+            st.markdown(response)
             
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response})
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
         
 
