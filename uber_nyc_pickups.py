9e47d8b1e90a8d6b89d725a60b469df8888b1d87
barckhoff <barckhoff@users.noreply.github.com>
2026-06-03 13:53:54 -0400

    Update uber_nyc_pickups.py

--- uber_nyc_pickups.py
+++ uber_nyc_pickups.py
@@ -36,8 +36,8 @@ if st.checkbox('Show bar chart'):
         data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
     st.bar_chart(hist_values)
 
-    # min: 0h, max: 23h, default: 17
-    hour_to_filter = st.slider('hour', 0, 23, 17)
+# min: 0h, max: 23h, default: 17
+hour_to_filter = st.slider('hour', 0, 23, 17)
 showmap = st.checkbox('Show map')
 if showmap:
     filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
